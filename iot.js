7058accf1bccb3643928a2fda67fff1b22efecf1
SuperBonny <bonora.matteo@gmail.com>
2016-03-27 19:00:11 +0200

    Fix port already in use error

--- iot.js
+++ iot.js
@@ -1,12 +1,14 @@
-var WebSocketServer = require('ws').Server
-    , wss = new WebSocketServer({
-        port: 8080
-    });
+var db = require('mysql');
+var server = require('ws').Server;
 
-wss.on('connection', function connection(ws) {
+var sockPort = 10611;
+
+var socket = new server({
+    port: sockPort
+});
+
+socket.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
         console.log('received: %s', message);
     });
-
-    ws.send('something');
 });
