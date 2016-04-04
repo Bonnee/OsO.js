ba5645409a563e67d731cd23bb98996e0354a95b
Matteo Bonora <bonora.matteo@gmail.com>
2016-04-04 18:17:30 +0200

    Update device schema

--- base.js
+++ base.js
@@ -1,10 +1,16 @@
 var db = require('mongoose');
 
-var schema = new db.Schema({
-    _id: String,
+var deviceSchema = new db.Schema({
+    _id: { type: String, lowercase: true },
     name: String,
     description: String,
     location: String,
-    dateAdd: Date,
+    dateAdd: { type: Date, default: Date.now },
     settings: [{ name: String, value: String }]
 });
+
+var Device = mongoose.model('Device', deviceSchema);
+
+this.addDevice = function() {
+  
+}
