b5a044a6c9030aac7d1ac410d5dc64c3713ab4c3
Matteo Bonora <bonora.matteo@gmail.com>
2016-04-14 18:57:55 +0200

    Add connected flag in db schema

--- base.js
+++ base.js
@@ -2,40 +2,42 @@ var mongoose = require('mongoose');
 var db = mongoose.connection;
 var Schema = mongoose.Schema;
 
-this.base = function (addr, name) {
+this.base = function(addr, name) {
     var self = this;
     var conn = 'mongodb://' + addr + '/' + name;
     mongoose.connect(conn);
 
     db.on('error', console.error.bind(console, 'Connection error:'));
-    db.once('open', function () {
+    db.once('open', function() {
         console.log('Connected to ' + conn);
     });
 
     var schema = new Schema({
         mac: {
-            type: String
-            , lowercase: true
-        }
-        , name: String
-        , description: String
-        , location: String
-        , dateAdd: {
-            type: Date
-            , default: Date.now
-        }
-        , settings: [Schema.Types.Mixed]
-        , data: [Schema.Types.Mixed]
+            type: String,
+            lowercase: true
+        },
+        name: String,
+        description: String,
+        location: String,
+        dateAdd: {
+            type: Date,
+            default: Date.now
+        },
+        connected: Boolean,
+        ,
+        settings: [Schema.Types.Mixed],
+        data: [Schema.Types.Mixed]
     }, {
         strict: false
     });
 
-    schema.statics.exists = function (addr, back) {
+    schema.statics.exists = function(addr, back) {
         addr = addr.toLowerCase();
 
         this.find({
             mac: addr
-        }, function (e, doc) {
+        }, function(e, doc) {
             if (doc.length > 0)
                 back(true);
             else
@@ -45,14 +47,14 @@ this.base = function (addr, name) {
 
     var Model = mongoose.model('devices', schema);
 
-    this.addDevice = function (manifest) {
+    this.addDevice = function(manifest) {
         var device = new Model(manifest);
-        device.save(device, function (e, data) {
+        device.save(device, function(e, data) {
             //console.log(data);
         });
     }
 
-    this.exists = function (mac, back) {
+    this.exists = function(mac, back) {
         return Model.exists(mac, back);
     }
 }
