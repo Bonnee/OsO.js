f057e4603e405736132388a4ec8807d7dc8899d1
Matteo Bonora <bonora.matteo@gmail.com>
2016-04-17 22:01:34 +0200

    Add initial logging feature

--- base.js
+++ base.js
@@ -3,59 +3,79 @@ var db = mongoose.connection;
 var Schema = mongoose.Schema;
 
 this.base = function(addr, name) {
-    var self = this;
-    var conn = 'mongodb://' + addr + '/' + name;
-    mongoose.connect(conn);
+	var self = this;
+	var conn = 'mongodb://' + addr + '/' + name;
+	mongoose.connect(conn);
 
-    db.on('error', console.error.bind(console, 'Connection error:'));
-    db.once('open', function() {
-        console.log('Connected to ' + conn);
-    });
+	db.on('error', console.error.bind(console, 'Connection error:'));
+	db.once('open', function() {
+		console.log('Connected to ' + conn);
+	});
 
-    var schema = new Schema({
-        mac: {
-            type: String,
-            lowercase: true
-        },
-        name: String,
-        description: String,
-        location: String,
-        dateAdd: {
-            type: Date,
-            default: Date.now
-        },
-        connected: Boolean,
-        settings: [Schema.Types.Mixed],
-        data: [Schema.Types.Mixed]
-    }, {
-        strict: false
-    });
+	var schema = new Schema({
+		mac: {
+			type: String,
+			lowercase: true
+		},
+		name: String,
+		description: String,
+		location: String,
+		dateAdd: {
+			type: Date,
+			default: Date.now
+		},
+		connected: Boolean,
+		settings: [Schema.Types.Mixed],
+		data: [Schema.Types.Mixed]
+	}, {
+		strict: false
+	});
 
-    schema.statics.exists = function(addr, back) {
-        addr = addr.toLowerCase();
+	schema.statics.exists = function(addr, back) {
+		addr = addr.toLowerCase();
 
-        this.find({
-            mac: addr
-        }, function(e, doc) {
-            if (doc.length > 0)
-                back(true);
-            else
-                back(false);
-        })
-    }
+		this.find({
+			mac: addr
+		}, function(e, doc) {
+			if (doc.length > 0)
+				back(true);
+			else
+				back(false);
+		});
+	}
 
-    var Model = mongoose.model('devices', schema);
+	var Model = mongoose.model('devices', schema);
 
-    this.addDevice = function(manifest) {
-        var device = new Model(manifest);
-        device.save(device, function(e, data) {
-            //console.log(data);
-        });
-    }
+	this.addDevice = function(mac, manifest) {
+		var device = new Model(manifest);
+		Model._id = mac;
+		device.save(device, function(e, data) {
+			if (e)
+				console.log(e);
+			console.log(data);
+		});
+	}
 
-    this.exists = function(mac, back) {
-        return Model.exists(mac, back);
-    }
+	this.exists = function(mac, back) {
+		return Model.exists(mac, back);
+	}
+
+	this.addRecord = function(mac, message) {
+		Model.findByIdAndUpdate(
+			mac, {
+				$push: {
+					"data": {
+						timestamp: Date.now,
+						data: message
+					}
+				}
+			},
+			function(err, model) {
+				if (err) console.log(err);
+				console.log(model);
+			}
+		);
+	}
 }
 
 module.exports = this.base;
--- oso.js
+++ oso.js
@@ -10,57 +10,57 @@ var base = require('./base.js');
 var db = new base('localhost', 'oso');
 
 server.on('connection', function(ws) {
-    var address = ws._socket.remoteAddress + ':' + ws._socket.remotePort;
-    var state = State.Connecting;
-    var devId;
+	var address = ws._socket.remoteAddress + ':' + ws._socket.remotePort;
+	var state = State.Connecting;
+	var devId;
 
-    console.log('Connection from: ' + address);
+	console.log('Connection from: ' + address);
 
-    ws.on('message', function(message) {
-        //console.log("Received: " + message);
-        message = JSON.parse(message);
+	ws.on('message', function(message) {
+		console.log("Received: " + message);
+		message = JSON.parse(message);
 
-        if (message.id == 'hello' && state == State.Connecting) { // Initial connection. Only accepted when state is 0
-            db.exists(message.data, function(ex) {
-                if (!ex) {
-                    state = State.Associating;
-                    ws.sendJSON('who', '');
-                    console.log(message.data + ' does not exist in db. Requesting data...');
-                } else { // Skip association
-                    state = State.Connected;
-                    console.log(message.data + ' recognized.');
-                }
-                devId = message.data;
-            });
-        } else if (message.id == 'who' && state = State.Associating) { // Adds a new device in db
-            state = State.Connected;
-            db.addDevice(JSON.parse(message.data));
-        } else if (message.id = 'data' && state = State.Connected) {
-            // Add code
-        } else {
-            console.log(address + ': ' + message.data);
-        }
-    });
+		if (message.id == 'hello' && state == State.Connecting) { // Initial connection. Only accepted when state is 0
+			db.exists(message.data, function(ex) {
+				if (!ex) {
+					state = State.Associating;
+					ws.sendJSON('who', '');
+					console.log(message.data + ' does not exist in db. Requesting data...');
+				} else { // Skip association
+					state = State.Connected;
+					console.log(message.data + ' recognized.');
+				}
+				devId = message.data;
+			});
+		} else if (message.id == 'who' && state = State.Associating) { // Adds a new device in db
+			state = State.Connected;
+			db.addDevice(devId, JSON.parse(message.data));
+		} else if (message.id = 'log' && state = State.Connected) {
+			addRecord(devId, JSON.parse(message.data));
+		} else {
+			console.log(devId + ': ' + message.data);
+		}
+	});
 
-    ws.on('close', function close() {
-        console.log(address + ' has disconnected.');
-    });
+	ws.on('close', function close() {
+		console.log(devId + ' has disconnected.');
+	});
 
-    function send(connection, data, id) {
-        var pkt = {
-            'id': id,
-            'data': data
-        }
-        connection.send(JSON.stringify(pkt));
-    }
+	function send(connection, data, id) {
+		var pkt = {
+			'id': id,
+			'data': data
+		}
+		connection.send(JSON.stringify(pkt));
+	}
 });
 
 var State = {
-    Connecting: 0,
-    Associating: 1,
-    Connected: 2,
-    Exchange: 3,
-    Error: 4
+	Connecting: 0,
+	Associating: 1,
+	Connected: 2,
+	Exchange: 3,
+	Error: 4
 }
 
 console.log('started.');
