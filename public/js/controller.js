f4b78fadf84435c9084041fb36869f23e8f93fb7
Matteo Bonora <bonora.matteo@gmail.com>
2016-06-01 23:21:29 +0200

    Fix routing

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,59 +1,63 @@
-var app = angular.module('OsO', ['ngRoute']);
+var app = angular.module('OsO', ['ui.router']);
 
-var devices;
-var selected;
-var def = $.Deferred();
+app.config(function($locationProvider, $urlRouterProvider, $stateProvider) {
 
-function init() {
-	$.getJSON("/devices", function(res) {
-		devices = res;
-		def.resolve(res);
-		console.log(devices);
+	$urlRouterProvider.deferIntercept();
+	$urlRouterProvider.otherwise('/');
+
+	$locationProvider.html5Mode({
+		enabled: false
 	});
-}
+	$stateProviderRef = $stateProvider;
+});
 
-app.controller('devices', ['$scope', '$http', 'ngRoute', function($scope, $http) {
-	def.promise();
+app.run(['$rootScope', '$state', '$stateParams',
+  function($rootScope, $state, $stateParams) {
+		$rootScope.$state = $state;
+		$rootScope.$stateParams = $stateParams;
+}]);
 
-	$scope.select(0); // Select the first tab to display something
+// This function dynamically adds all the views to the page and starts the router
+app.run(['$q', '$rootScope', '$http', '$urlRouter',
+  function($q, $rootScope, $http, $urlRouter) {
+		$http.get('/devices').success(function(data) { // Fetches the devices through REST
+			$rootScope.devices = data;
+			angular.forEach(data, function(value, key) {
+				var state = value;
+				state["url"] = "^/" + value.id;
+				state["views"] = {
+					name: "main",
+					templateUrl: "devices/" + value.id + "/index.html"
+				}
+				$stateProviderRef.state(value.id, value);
+			});
+			$urlRouter.sync();
+			$urlRouter.listen();
+		});
+}]);
 
+app.controller('getDevices', ['$scope', '$http', '$state', function($scope, $http, $state) {
 	$scope.url = '';
 
 	$scope.getSelected = function() { // Returns the currently selected device.
-		return devices[selected];
-	}
-
-	$scope.getDevices = function() { // Returns all the devices.
-		return devices;
+		return $scope.devices[$scope.selected];
 	}
 
 	$scope.select = function(i) { // Changes the selected device
-		selected = i;
 
-		//$scope.url = devices[selected].url;
+		$scope.selected = i;
+		$state.go($scope.devices[i].name);
 	}
 
 	$scope.isActive = function(i) {
-		if ($scope.getSelected() == devices[i])
+		if ($state.$current.id == $scope.devices[i].id)
 			return true;
 		return false;
 	}
 
+	//$scope.select(0); // Select the first tab to display something
 }]);
 
-app.config(['$routeProvider', '$locationProvider',
-  function($routeProvider, $locationProvider) {
-		def.promise();
-		for (var i = 0; i < devices.length; i++) {
-			$routeProvider.when('/pub' + devices[i].id, {
-				templateUrl: 'devices/' + devices[i].id + "/index.html",
-				controller: 'Ctrl'
-			});
-		}
-		$routeProvider.otherwise({
-			redirectTo: '/pub'
-		});
+app.controller('main', function($scope) {
 
-		$locationProvider.html5Mode(true);
-	}
-]);
+});
